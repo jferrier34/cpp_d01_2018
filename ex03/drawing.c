bf6d52a4cff6c2af523ef1700253c4cb82200f64
Julien Ferrier <jferrier@localhost.localdomain>
2019-01-04 20:08:49 +0100

    [modify] the third exercise is tested and work I corrig the loops

--- ex03/drawing.c
+++ ex03/drawing.c
@@ -14,9 +14,11 @@
 
 void draw_square(uint32_t **img, const point_t *origin,
 size_t size, uint32_t color) {
-    for (size_t i = origin->y; i < size; i++) {
-        for (size_t j = origin->x; j < size; j++) {
-            img[i][j] = color;
-        }
-    }
+    size_t x = origin->x;
+    size_t y = origin->y;
+
+    for (y ; y < origin->y + size; ++y)
+        for (x; x < origin->x + size; ++x)
+            img[y][x] = color;
 }
+
